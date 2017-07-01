082c0cfe6f5cc3a4fcac09bae4f5edadf309fc0e
Zelig880 <scuomo1988@gmail.com>
2017-07-01 21:45:01 +0100

    Add basic information on the menu state

--- src/StormBasket/js/Menu.js
+++ src/StormBasket/js/Menu.js
@@ -2,15 +2,36 @@ class Menu extends Phaser.State {
 
 	create() {
         
-         var nameLabel = this.add.text(
+        this.add.text(
             100, 
             50, 
             "Storm Basket",
             {
-                font: "50px Arial",
+                font: "50px",
                 fill: "#999999"
             }
-        )
+        );
+
+        this.add.text(
+            100,
+            150,
+            "Indie game about basketball. \nScore as many basket as possible to achieve the highest score.",
+            {
+                font: "24px",
+                fill: "#999999"
+            }
+        );
+
+        this.add.text(
+            100,
+            250,
+            "PRESS SPACE TO BEGIN",
+            {
+                font: "24px",
+                fill: "yellow"
+            }
+
+        );
 
         var spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
