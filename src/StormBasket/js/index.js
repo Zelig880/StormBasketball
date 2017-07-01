9497aa470aeee1c411ab3b175be00c207f7d9417
Zelig880 <scuomo1988@gmail.com>
2017-07-01 21:34:47 +0100

    Make small changes to the game window layout and size

--- src/StormBasket/js/index.js
+++ src/StormBasket/js/index.js
@@ -4,17 +4,18 @@ const boot = new Boot();
 const preload = new Preload();
 const menu = new Menu();
 const main = new Main();
+const gameOver = new GameOver();
 
 class Game extends Phaser.Game {
 
     constructor() {
 
-        super(500, 500, Phaser.AUTO, 'area');
+        super(800, 600, Phaser.AUTO, 'gameArea');
         this.state.add('Boot', boot, false);
         this.state.add('Preload', preload, false);
         this.state.add('Menu', menu, false);
         this.state.add('Main', main, false);
-        //this.state.add('GameOver', gameOver, false);
+        this.state.add('GameOver', gameOver, false);
         
         this.state.start('Boot');
     }
